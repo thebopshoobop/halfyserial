969cd9ecd9f849a5600784741f135c8ee1cad1f6
Will <will.timpson@gmail.com>
2015-12-29 03:15:45 -0500

    Functions! A start, anyway...
    signaler(message): Send message to halfy and return the response
    
    get_status(): Send status commands for each output to signaler()
    and print the responses. Needs mad work, obvi...

--- multi-read.py
+++ multi-read.py
@@ -7,7 +7,23 @@ def listener(): # Run as a separate process to read from the serial port
     while True: # Loop forever
         i = halfy.read(1) # Attempt to read one byte from the serial port
         q.put(i.decode()) # Decode from byte to character and add to queue
+
+def signaler(message): # Send a command to the halfy and return the response
+    output = []
+    halfy.write(message) # Write the status command to the serial port
+    p.join(.0625) # Wait (1/16th of a second) for the response to accumulate in the queue
     
+    # Translate the queue into a list
+    while not q.empty():
+        output.append(q.get())
+
+    return ''.join(output) # Concatenate ('' delimited) the character list and return
+
+def get_status(): # Read and print status
+    for i in [ b'SL1O1T', b'SL1O2T', b'SL1O3T', b'SL1O4T' ]: # Iterate over the output status commands
+        status = signaler(i)
+        print(status)
+
 if __name__ == '__main__':
     halfy = serial.Serial('/dev/ttyUSB0') # Initialize serial port
     q = mp.Queue() #Initialize queue to talk to listener process
@@ -16,17 +32,9 @@ if __name__ == '__main__':
     p = mp.Process(target=listener)
     p.start()
     
-    # Read and print status
-    for i in [ b'SL1O1T', b'SL1O2T', b'SL1O3T', b'SL1O4T' ]: # Iterate over the output status commands
-        output = []
-        halfy.write(i) # Write the status command to the serial port
-        p.join(.0625) # Wait (1/16th of a second) for the response to accumulate in the queue
-        # Translate the queue into a list
-        while q.empty() == False:
-            output.append(q.get())
-        print(''.join(output)) # Concatenate ('' delimited) the character list and print
+    get_status()
     
-    # If thread is still active
+    # If process is still active
     if p.is_alive():
         print("Done.")
 
