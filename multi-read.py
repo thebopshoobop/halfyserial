f6cad05f59ddf7c260369982f128fae3fc0bbee4
Will <will.timpson@gmail.com>
2015-12-31 03:33:12 -0500

    Pimp My Status Funtion(s)
    * Mew method: get_single_status()
    * Allow inputs or outputs to be selected when calling either status
    method by passing the appropriate segment object
    * Default to returning statuses for the output segment
    * Build the status command in the loop with the variables provided
    in the config file

--- multi-read.py
+++ multi-read.py
@@ -19,19 +19,31 @@ def signaler(message): # Send a command to the halfy and return the response
 
     return ''.join(output) # Concatenate ('' delimited) the character list and return
 
-def get_status(): # Read and print status
-    for i in [ b'SL1O1T', b'SL1O2T', b'SL1O3T', b'SL1O4T' ]: # Iterate over the output status commands
-        status = signaler(i)
-        print(status)
+def get_status(segment = outputs): # Read and print input or output statuses
+    # Generate and excecute status comands
+    for i in range(segment):
+        port = i + 1 # Switch to 1-indexed range
+        get_single_status(port, segment)
+
+def get_single_status(port, segment = outputs):
+    segment_char = 'O' # Default to checking output
+    if segment is inputs: # Override default and check input
+        segment_char = 'I'
+
+    command_list = 'SL', str(level), segment_char, str(port), 'T' # Make a list of strings
+    command_string = "".join(command_list) # Concatenate into a single string
+    command_bytes = command_string.encode('ascii') # Turn that string into ascii bytes
+    status = signaler(command_bytes) # Signal the halfy
+    print(status)
 
 if __name__ == '__main__':
     # Load values from settings file
     with open('config.json') as json_data_file:
-        data = json.load(json_data_file)
-        device_name = data['device_name']
-        inputs = int(data['inputs'])
-        outputs = int(data['outputs'])
-        level = int(data['level')
+        data = json.load(json_data_file) # Make a dictionary from the config file data
+        device_name = data['device_name'] # Serial port in use: string ['/dev/tty*']
+        inputs = int(data['inputs']) # Number of inputs in use: int [1-8]
+        outputs = int(data['outputs']) # Number of outputs in use: int [1-4]
+        level = int(data['level') # Matrix level in use: int [1-2]
 
     try:
         halfy = serial.Serial(device_name) # Attempt to initialize serial port
