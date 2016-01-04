82cf07c38734d0d6422001ab26fcdf975092c208
Will <will.timpson@gmail.com>
2016-01-04 01:38:03 -0500

    Config stays in the dictionary. Inputs, Outputs are dictionaries.
    * The config values stay in the config{} dictionary. This makes for
    less passing of variables around and makes references to config
    variables explicit. As before, the inputs, outputs, and levels
    have to be made integers.
    
    * The inputs and outputs config keys now contain dictionaries of
    the format {[port number]:[label]}. This allows for gaps in the
    ports used. Instead of iterating over the ports with range(), we
    can directly iterate over the dictionaries and we can ask if a
    port is in the inputs or outputs objects. We can also use the
    label values when presenting our results.

--- multi-read.py
+++ multi-read.py
@@ -28,17 +28,16 @@ def signaler(message): # Send a command to the halfy and return the response
 
 def get_status(): # Read and print output statuses
     status = {} # Dictionary of statuses
-    for i in range(outputs): # Iterate over the outputs
-        port = i + 1 # Switch to 1-indexed range
+    for output_port in sorted(config['outputs']): # Iterate over the outputs
         try:
-            status.update(get_single_status(port)) # Attempt to add a status entry to the dictionary
+            status.update(get_single_status(output_port)) # Attempt to add a status entry to the dictionary
         except:
             raise
     return status
 
-def get_single_status(port):
-    if 1 <= port <= outputs:
-        command_list = 'SL', str(level), 'O', str(port), 'T' # Make a list of strings
+def get_single_status(port): # Get the input connected to a given output port
+    if port in config['outputs']: # Make sure port is an active output
+        command_list = 'SL', str(config['level']), 'O', str(port), 'T' # Make a list of strings
         halfy_response = build_command(command_list) # Signal the halfy
         response_list = re.split(r'[\(|\ \)]', halfy_response) # Break the response string into a list
         if check_command(response_list.pop(0)): # Remove the command response and check that it succeeded
@@ -54,10 +53,10 @@ def get_single_status(port):
         raise ConfigError("output port is not within active range:", port)
 
 def set_single_status(out_port, in_port):
-    if 1 <= out_port <= outputs and 1 <= in_port <= inputs:
-        command_list = 'CL', str(level), 'I', str(in_port), 'O', str(out_port), 'T'
-        halfy_response = build_command(command_list)
-        if check_command(halfy_response):
+    if out_port in config['outputs'] and in_port in config['inputs']: # Make sure that our ports are active
+        command_list = 'CL', str(config['level']), 'I', str(in_port), 'O', str(out_port), 'T' # Make a list of strings
+        halfy_response = build_command(command_list) # Signal the halfy
+        if check_command(halfy_response): # Make sure our command was successful
             return True
         else:
             raise ConfigError("command was not successfully implemented:", halfy_response)
@@ -72,35 +71,40 @@ def check_command(response): # Make sure that the command was successfully excec
     else:
         return False
 
-def build_command(command_list): 
+def build_command(command_list): # Generate a command for the halfy, excecute it, and return the result
     command_string = "".join(command_list) # Concatenate into a single string
     command_bytes = command_string.encode('ascii') # Turn that string into ascii bytes
-    return signaler(command_bytes) # Signal the halfy
+    return signaler(command_bytes) # Signal the halfy and return
 
 def parse_config():
     # Load values from config file
     with open('config.json') as json_data_file:
-        data = json.load(json_data_file) # Make a dictionary from the config file data
-    # Sanitize config file variables
-    device_name = data['device_name'] # Serial port in use: string ['/dev/tty*']
-    if not os.path.exists(device_name):
-        raise ConfigError("device_name does not exist [/dev/tty*]:", device_name)
-    inputs = int(data['inputs']) # Number of inputs in use: int [1-8]
-    if not 1 <= inputs <= 8:
-        raise ConfigError("inputs is out of the allowed range [1-8]:", inputs)
-    outputs = int(data['outputs']) # Number of outputs in use: int [1-4]
-    if not 1 <= outputs <= 4:
-        raise ConfigError("outputs is out of the allowed range [1-4]:", outputs)
-    level = int(data['level']) # Matrix level in use: int [1-2]
-    if not 1 <= level <= 2:
-        raise ConfigError("level is out of the allowed range [1-2]:", level)
+        config = json.load(json_data_file) # Make a dictionary from the config file data
 
-    return device_name, inputs, outputs, level
+    # Make our string dictionary entries into integers
+    config['level'] = int(config['level'])
+    for port in 'inputs', 'outputs': 
+        for key in config[port]: # Iterate over the keys in the input/output dictionaries
+            config[port][int(key)] = config[port].pop(key) # Create a new dictionary item whose key is an integer
+
+    # Sanitize config file variables
+    if not os.path.exists(config['device_name']): # Serial port in use: string ['/dev/tty*']
+        raise ConfigError("device_name does not exist [/dev/tty*]:", config['device_name'])
+    for input_number in config['inputs']:
+        if not 1 <= input_number <= 8: # Number of inputs in use: int [1-8]
+            raise ConfigError("inputs is out of the allowed range [1-8]:", config['inputs'])
+    for output_number in config['outputs']:
+        if not 1 <= output_number <= 4: # Number of outputs in use: int [1-4]
+            raise ConfigError("outputs is out of the allowed range [1-4]:", config['outputs'])
+    if not 1 <= config['level'] <= 2: # Matrix switcher level in use: int [1-2]
+        raise ConfigError("level is out of the allowed range [1-2]:", config['level'])
+
+    return config
 
 if __name__ == '__main__':
     try:
-        device_name, inputs, outputs, level = parse_config() # Parse and sanitize variables from config file
-        halfy = serial.Serial(device_name) # Attempt to initialize serial port
+        config = parse_config() # Parse and sanitize variables from config file
+        halfy = serial.Serial(config['device_name']) # Attempt to initialize serial port
     # Ensure that the config file is parsed properly and the serial port was initialized
     except serial.serialutil.SerialException as err:
         print("Serial Port Error:", err)
@@ -119,20 +123,18 @@ if __name__ == '__main__':
         p = mp.Process(target=listener)
         p.start()
         try:
-            set_single_status(3, 1)
+            set_single_status(1, 7)
             out_dict = get_status()
-            for key in range(outputs):
-                if not out_dict[key + 1]:
-                    print("Output", key + 1, "is not connected")
+            for output_port in sorted(config['outputs']):
+                if not out_dict[output_port]:
+                    print(config['outputs'][output_port], "is not connected")
                 else:
-                    print("Output", key + 1, "is connected to Input", out_dict[key + 1])
+                    print(config['outputs'][output_port], "is connected to", config['inputs'][out_dict[output_port]])
         except ConfigError as err:
             print("Command error:", err.error_message, err.incorrect_value)
 
-        # If process is still active
+        # If process is still active"Output"
         if p.is_alive():
-            print("Done.")
-
             # Terminate
             p.terminate()
             p.join()
